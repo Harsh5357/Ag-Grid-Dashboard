cdbef80900510573c28b52b22c6519d44972a6ab
Harsh5357 <harsh5357harshvish@gmail.com>
2025-11-30 16:35:16 +0530

    Add files via upload

---
+++ _sdk/data_sdk.js
@@ -0,0 +1,10 @@
+// Minimal placeholder for data_sdk.js
+// Replace with your real data SDK code.
+(function(){
+  console.log('[placeholder] data_sdk loaded');
+  window.dataSDK = {
+    fetchEmployees: function(){
+      return Promise.resolve([]);
+    }
+  };
+})();
---
+++ _sdk/element_sdk.js
@@ -0,0 +1,8 @@
+// Minimal placeholder for element_sdk.js
+// Replace with your production SDK implementation.
+(function(){
+  console.log('[placeholder] element_sdk loaded');
+  window.elementSDK = {
+    init: function(){ console.log('elementSDK.init() called'); }
+  };
+})();
